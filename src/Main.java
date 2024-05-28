928edbd552ac397fbbd7d9cc9e6a69d80113d853
Oleg Somov <doremi@fasollasi.ru>
2024-05-28 20:39:36 +0300

    Added a class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 2040;
-        int mile = 20;
-        int bonus = ticket / mile;
-        System.out.println("За покупку билета Вам будет начисленно: " + bonus + " бонусов!");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println("За покупку билета Вам будет начисленно: " + miles + " бонусов!");
     }
 }
