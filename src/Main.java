95f7edd7288cd0b17a0f4f4788fc7dce2c48d628
Oleg Somov <doremi@fasollasi.ru>
2024-05-28 20:39:36 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         int ticket = 2040;
         int mile = 20;
         int bonus = ticket / mile;
-            System.out.println("За покупку билета Вам будет начисленно: " + bonus + " бонусов!");
-        }
+        System.out.println("За покупку билета Вам будет начисленно: " + bonus + " бонусов!");
     }
+}
